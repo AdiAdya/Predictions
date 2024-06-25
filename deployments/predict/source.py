935730930d547b3d67f5ff27b5e8cfb519629fab
Aditya Adya <adityaadyamk@gmail.com>
2024-06-25 12:09:55 +0000

    Deployed `predict`

--- deployments/predict/source.py
+++ deployments/predict/source.py
@@ -6,13 +6,12 @@ from tensorflow import keras
 Model = keras.models.load_model('data/Model.h5')
 
 # main function
-async def predict(query):   
-    prediction = await Model.predict([query])
+def predict(query):   
+    prediction = Model.predict([query])
     print(prediction)
     return prediction
 
 # to run locally via git & terminal, uncomment the following lines
 # if __name__ == "__main__":
-#   import asyncio
-#   result = asyncio.run(predict(...))
+#   result = predict(...)
 #   print(result)
