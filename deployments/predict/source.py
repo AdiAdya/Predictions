78430733877f8529c5df5f4523df29045335e338
Aditya Adya <87091900+AdiAdya@users.noreply.github.com>
2024-06-25 13:32:41 +0100

    Update source.py

--- deployments/predict/source.py
+++ deployments/predict/source.py
@@ -1,15 +1,13 @@
-import modelbit, sys
-from typing import *
-from tensorflow import keras
+import tensorflow as tf
+import tensorflow_hub as hub
+Model = tf.keras.models.load_model('my_model.h5',custom_objects={'KerasLayer':hub.KerasLayer})
 
 
-Model = keras.models.load_model('data/Model.h5')
-
-# main function
 def predict(query):   
     prediction = Model.predict([query])
     print(prediction)
     return prediction
+
 
 # to run locally via git & terminal, uncomment the following lines
 # if __name__ == "__main__":
