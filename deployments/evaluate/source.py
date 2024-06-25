c7789d925e9f8588907dfa4774ed1cb8a7419421
Aditya Adya <adityaadyamk@gmail.com>
2024-06-25 11:36:22 +0000

    Deployed `evaluate`

---
+++ deployments/evaluate/source.py
@@ -0,0 +1,18 @@
+import modelbit, sys
+from typing import *
+from tensorflow import keras
+
+
+Model = keras.models.load_model('data/Model.h5')
+
+# main function
+async def evaluate(query):
+    prediction = await Model.predict(query)
+    print(prediction)
+    return prediction
+
+# to run locally via git & terminal, uncomment the following lines
+# if __name__ == "__main__":
+#   import asyncio
+#   result = asyncio.run(evaluate(...))
+#   print(result)
