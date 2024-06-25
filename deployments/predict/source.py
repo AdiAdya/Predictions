3261ef477fcf786d43259603e9cdc1efdf6d1a64
Aditya Adya <87091900+AdiAdya@users.noreply.github.com>
2024-06-25 13:34:08 +0100

    Update source.py

--- deployments/predict/source.py
+++ deployments/predict/source.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 import tensorflow_hub as hub
-Model = tf.keras.models.load_model('my_model.h5',custom_objects={'KerasLayer':hub.KerasLayer})
+Model = tf.keras.models.load_model('data/Model.h5',custom_objects={'KerasLayer':hub.KerasLayer})
 
 
 def predict(query):   
